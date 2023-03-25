1f18f21e57430e419bf26f085a0c2f5b35befec7
yy <yy>
2023-03-26 01:56:56 +0300

    ex

--- task_2.py
+++ task_2.py
@@ -12,11 +12,11 @@ def last_name(n_people: int, k_number: int) -> int:
     if n_people == 1:
         return people_list[0]
     else:
-        people_list.pop(k_number % n_people - 1)
+        people_list.pop(k_number % n_people - 1) # удаляем выбывшего "-1" так как список начинается с 0-индекса, а порядковый номер с 1
     return last_name(n_people - 1, k_number)
 
 if __name__ == "__main__":
-    people_list = ["Коля", "Оля", "Вика", "Саша", "Витя"]
+    people_list = ["Коля", "Света", "Ольга", "Саша", "Витя"]
     people_list_copy = people_list.copy() #копия списка создана для того что бы узнать порядковый номер человека
     print(f'Список людей: {" ".join(people_list)}')
     slog = int(input(f'Укажите кол-во слогов: '))
